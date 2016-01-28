024d49f7be11b4b8e177c7d0260d4cd2ff9b2b45
DanielRavina <danielravina@gmail.com>
2016-01-27 19:55:47 -0800

    last commit

--- src/js/components/Home/Row.jsx
+++
@@ -1,9 +0,0 @@
-var React       = require('react');
-var Row = React.createClass({
-  render: function() {
-    return <div className={"row"}>{this.props.children}</div>
-  }
-})
-
-
-module.exports = Row;
